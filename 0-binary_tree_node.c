3e9d7ab3a3eb58024ed50139b01a605771c81bc4
Ian Culp <icculp@gmail.com>
2020-07-20 20:43:17 +0000

    Adding 0 and 1

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - creates a binary tree node
+* @parent: parent node pointer
+* @value: value to put in node
+* Return: pointer to new node, or NULL on failure
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_left - inserts a node to the left
+* @parent: parent node pointer
+* @value: value to put in node
+* Return: pointer to new node, or NULL on failure
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL || parent == NULL)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	else
+	{
+		parent->left = new;
+	}
+	return (new);
+}
