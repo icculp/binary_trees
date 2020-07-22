3046247adc8fa8c95f404d3bbd39d9d134c42a90
Ian Culp <icculp@gmail.com>
2020-07-22 23:19:08 +0000

    104

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_rotate_right - rotate right
+* @tree: tree is actually the node pointer
+* Return: pointer to new root
+*/
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *p, *pr;
+
+	if (tree == NULL)
+		return (0);
+	p = tree->left;
+	p->parent = tree->parent;
+	pr = p->right;
+	p->right = tree;
+	tree->parent = p;
+	tree->left = pr;
+	if (pr)
+		pr->parent = p;
+	return (p);
+}
