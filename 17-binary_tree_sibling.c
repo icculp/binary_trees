a079475697ea4411c7d5f8ec74ddb147970f23ed
Ian Culp <icculp@gmail.com>
2020-07-22 03:44:31 +0000

    17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - return node of same level and parent
+* @node: node pointer
+* Return: pointer to node
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
