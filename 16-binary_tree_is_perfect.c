67b0b7a74444b40d8c517829b01ee6c519d49128
Ian Culp <icculp@gmail.com>
2020-07-23 18:20:58 +0000

    16 for searn and jill

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,30 +1,20 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_full - function
-* @tree: tree to check
-* Description: check if a binary tree is full
-* Return: 1 if full, 0 otherwise
+* binary_tree_is_full - check if tree is full
+* @tree: tree pointer
+* Return: 1 or 0
 */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	if (tree->left != NULL)
-		if (binary_tree_is_full(tree->left) == 0)
-			return (0);
-
-	if (tree->right != NULL)
-		if (binary_tree_is_full(tree->right) == 0)
-			return (0);
-
-	if (tree->left != NULL && tree->right != NULL)
+	if (!tree->left && !tree->right)
 		return (1);
-	else if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	else
-		return (0);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
 
 /**
