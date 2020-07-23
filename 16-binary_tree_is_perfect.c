1af0d7db91b93c8d4d023f570c2bd406b89e9cfa
Ian Culp <icculp@gmail.com>
2020-07-23 18:19:46 +0000

    16 for searn and jill

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,59 +1,75 @@
 #include "binary_trees.h"
 
 /**
-* bdepth - measure depth of left branch
-* @tree: tree pointer
-* Return: height
+* binary_tree_is_full - function
+* @tree: tree to check
+* Description: check if a binary tree is full
+* Return: 1 if full, 0 otherwise
 */
-
-int bdepth(const binary_tree_t *tree)
-{
-	int d = 0;
-
-	while (tree)
-	{
-		d++;
-		tree = tree->left;
-	}
-	return (d);
-}
-
-/**
-* perfect_recursion - recursive algorithm for checking if btree is perfect
-* @tree: tree pointer
-* @d: depth of left branch
-* @level: level of tree
-* Return: 1 or 0
-*/
-
-int perfect_recursion(const binary_tree_t *tree, int d, int level)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (true);
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (d == (level + 1));
-	}
-	if (tree->left == NULL || tree->right == NULL)
-		return (false);
-	return (perfect_recursion(tree->left, d, level + 1) &&
-		perfect_recursion(tree->right, d, level + 1));
+		return (0);
+
+	if (tree->left != NULL)
+		if (binary_tree_is_full(tree->left) == 0)
+			return (0);
+
+	if (tree->right != NULL)
+		if (binary_tree_is_full(tree->right) == 0)
+			return (0);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (1);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (0);
 }
 
 /**
-* binary_tree_is_perfect - check if tree is perfect
-* @tree: tree pointer
-* Return: 1 or 0
+* balance - function
+* @tree: ptr to root of tree to traverse through
+* @counter: variable to store traverseal steps
+* @max: variable to store recursive_height
+* Description: gets height of tree
+* Return: height of tree
 */
+int balance(const binary_tree_t *tree, int counter, int max)
+{
+	if (tree->left != NULL)
+		max = balance(tree->left, counter + 1, max);
 
+	if (tree->right != NULL)
+		max = balance(tree->right, counter + 1, max);
+
+	if (max < counter)
+		max = counter;
+	return (max);
+}
+
+/**
+* binary_tree_is_perfect - function
+* @tree: tree to check
+* Description: check if a binary tree is balanced and full
+* Return: 1 if perfect, 0 otherwise
+*/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int d;
+	int counterL = 0, counterR = 0;
 
 	if (tree == NULL)
 		return (0);
-	d = bdepth(tree);
-	if (perfect_recursion(tree, d, 0))
+
+
+	if (tree->left != NULL)
+		counterL = balance(tree->left, 1, 1);
+
+	if (tree->right != NULL)
+		counterR = balance(tree->right, 1, 1);
+
+	if (counterL - counterR == 0 && binary_tree_is_full(tree))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
